d88841524c7a7f42afbf67578c31baad016cadb4
potsables <postables@rtradetechnologies.com>
2018-10-25 17:49:38 -0700

    scripts: change ethminer default to ethermine

--- scripts/ethminer_start.sh
+++ scripts/ethminer_start.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
 
-USERNAME="......"
+USERNAME="..."
 WORKER_NAME=$(hostname)
-PORT="20535"
-URL="stratum2+tcp://$USERNAME.$WORKER_NAME:x@us-east.ethash-hub.miningpoolhub.com:$PORT"
+PORT="4444"
+URL="stratum+tcp://$USERNAME.$WORKER_NAME:x@us1.ethermine.org:$PORT"
 API="no"
 
 if [[ "$API" == "yes" ]]; then
-    ethminer -U -P "$URL" --api-port -6767 2>&1 | tee --append "$HOME/ethminer.log"
-else
-    ethminer -U -P "$URL" 2>&1 | tee --append "$HOME/ethminer.log"
+	    ethminer -U -P "$URL" --api-port -6767 2>&1 | tee --append "$HOME/ethminer.log"
+    else
+	        ethminer -U -P "$URL" 2>&1 | tee --append "$HOME/ethminer.log"
 fi
