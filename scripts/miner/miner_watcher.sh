de9f6d41c002b3add43a7969a5440eff982e8247
Alex Trottier <postables@rtradetechnologies.com>
2019-12-13 13:16:22 -0800

    scripts/miner: adjust equality comparison to string match

--- scripts/miner/miner_watcher.sh
+++ scripts/miner/miner_watcher.sh
@@ -5,7 +5,7 @@
 while true; do
     echo "[INFO] checking stratum address not supported"
     OUT=$(sudo systemctl status miner | grep -c "Address family not supported by protocol")
-    if [[ "$OUT" -gt 0 ]]; then
+    if [[ "$OUT" != "0" ]]; then
         echo "[ERROR] encountered ipv6 stratum addressing issue, restarting miner"
         sudo systemctl restart miner
     fi
@@ -19,8 +19,9 @@ while true; do
     echo "[INFO] checking nvidia driver status"
     OUT=$(nvidia-smi | grep -c "Unable to determine")
     # if this is true then it means the driver crashed and we need to reboot the system
-    if [[ "$OUT" -gt 0 ]]; then
+    if [[ "$OUT" != "0" ]]; then
         sudo reboot now
     fi
+    
     sleep 30
 done
